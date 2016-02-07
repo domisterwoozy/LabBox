eaef58429ce156a1e0161c9c7d002c7b9a0b038f
Jacob Schneider <jacobschneiderjgs@gmail.com>
2016-02-07 16:57:15 -0500

    more scalar field extensions

--- Math3D/VectorCalc/ScalarFieldExtensions.cs
+++ Math3D/VectorCalc/ScalarFieldExtensions.cs
@@ -9,8 +9,27 @@ namespace Math3D.VectorCalc
 {
     public static class ScalarFieldExtensions
     {
+        private const double dummyExponent = 10; // when the coefficient is zero the exponent does not matter but still needs to exist and cannot be zero or one
+
+        /// <summary>
+        /// A scalar field that varies solely with the distance from the origin.
+        /// The value of the field is strength*radius^power.
+        /// </summary>
+        public static IScalarField SphericalField(double strength, double power) => new SphericalScalarField(Tuple.Create(strength, 0.0, 0.0), Tuple.Create(power, dummyExponent, dummyExponent));
+
+        /// <summary>
+        /// Returns a new scalar field only active inside a specified radius of influence.
+        /// </summary>
         public static IScalarField Within(this IScalarField f, double radOfInf) => new ClampedScalarField(f, pos => pos.MagSquared <= radOfInf * radOfInf);
+
+        /// <summary>
+        /// Adds this scalar field to another.
+        /// </summary>
         public static IScalarField Add(this IScalarField f, IScalarField other) => new SumScalarField(f, other);
-        public static IScalarField Translate(this IScalarField f, Vector3 translation) => new TranslatedScalarField(f, translation);      
+
+        /// <summary>
+        /// Returns a new scalar field with the origin moved to a specified position.
+        /// </summary>
+        public static IScalarField Translate(this IScalarField f, Vector3 translation) => new TranslatedScalarField(f, translation);             
     }
 }
--- Physics3D/Forces/Potentials.cs
+++ Physics3D/Forces/Potentials.cs
@@ -10,9 +10,6 @@ namespace Physics3D
 {
     public static class Potentials
     {
-        private const double dummyExponent = 10; // when the coefficient is zero the exponent does not matter but still needs to exist and cannot be zero or one
-
-        public static IScalarField InverseSquare(double strength) => CentralForce(strength, -1);
-        public static IScalarField CentralForce(double strength, double power) => new SphericalScalarField(Tuple.Create(-strength, 0.0, 0.0), Tuple.Create(power, dummyExponent, dummyExponent));        
+        public static IScalarField InverseSquare(double strength) => ScalarFieldExtensions.SphericalField(-strength, -1);      
     }
 }
